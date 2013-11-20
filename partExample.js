e4b797493e5d1cf499d5fe67a91636bbed8d7f54
Damian Axford <damian@axford.me.uk>
2013-11-20 14:19:53 +0000

    added catalogue and dimension concepts

--- partExample.js
+++ partExample.js
@@ -25,6 +25,17 @@ Screw.specification.addParam({name:'r',
 // compact form, using alias for specification and short-hand parameter definition
 Screw.spec.add('l','length','mm',Number,true,8);
 
+// define catalogue source(s), source refers to a resource in the project
+// if referenced as a part catalogue, the host will automatically match up
+// the resource file to the part specification, etc and provide a friendly editor
+// for the specification data
+// the load function performs a lazy load, actually executed by the host
+Screw.catalogue.load({src:'Screws.csv',autoCreate:true, includeExample:true});
+
+// the catalogue can also be programmatically populated, specified values will
+// be merged with defaults from the specification
+Screw.catalogue.add({r:5, l:20});
+
 
 // Make
 // -----------
@@ -35,14 +46,36 @@ Screw.prototype.make = function(spec) {
 	// the specification object is bound into the new part for later reference
 	// common parameters are automatically extracted, e.g. material
 	var part = new Part(spec);
+	
+	// fetch manufacturing process information from the host
+	// a process is a set of specification info, e.g. tolerances
+	var process = getProcess('3DPrint');
 
 	// do some CSG stuff to make a screw based on the spec
 	var cube = CSG.roundedCube({radius: 10, roundradius: 2, resolution: 16});
   	var sphere = CSG.sphere({radius: 10, resolution: 16}).translate([5, 5, 5]);
   	var csg = cube.union(sphere);
 
+	// define connectors
 	part.connectors.add({name:'Base',pos:[0,0,0],dir:[0,0,-1]});
-	part.connectors.add({name:'Tip',pos:[0,0,-spec.l],dir:[0,0,-1]});
+	part.connectors.add({name:'Tip',pos:[0,0,-spec.l.value],dir:[0,0,-1]});
+	
+	
+	// define dimensions
+	// can optionally be linked to a specification parameter
+	// which allows for friendly annotations
+	part.dimensions.addLinear({start:part.connectors.Base.pos, 
+							   end:part.connectors.Tip.pos, 
+							   param:spec.l,
+							   offset: spec.l.value,
+							   offsetDir = [1,0,0]);
+	
+	// dimension lies in plane of circle, as defined by normal
+	part.dimensions.addRadius({origin:part.connectors.Base.pos,
+							   normal: [0,0,1], 
+							   radius:spec.r.value, 
+							   param:spec.r,
+							   offset: 5);
 	
 	
 	part.fromCSG(csg);
@@ -79,6 +112,11 @@ ScrewAndWasher.parts = ['Screw','Washer'];
 // the prototype also includes common values such as supplier, labour cost, labour time
 // the specification can be extended in the same was a part specification
 
+// A catalogue can also be defined in the same way as for parts
+ScrewAndWasher.catalogue.load({src:'ScrewsAndWashers.csv',autoCreate:false, includeExample:false});
+ScrewAndWasher.catalogue.add({'ScrewName':'M4CapScrew', 'WasherName':'M4PennyWasher'});
+
+
 // make the assembly
 ScrewAndWasher.make = function(spec) {
 
@@ -88,11 +126,18 @@ ScrewAndWasher.make = function(spec) {
 	var screw = getPart(spec.ScrewName);
 	var washer = getPart(spec.WasherName);
 	
+	// can get sub-assemblies in a similar way
+	// var b = getAssembly('SubAssembly1');
+	
 	// connect them, connect(partToConnect, self.connector, partToConnect.connector) 
 	a.add(washer).connect(screw,'top','base');
 
 	// define connectors for the assembly, in this case copying from one of the parts
 	a.connectors.add(washer.connector('bottom'));
+	
+	
+	// dimensions can be defined in same manner as for parts
+	
 
 	return a;
 }
